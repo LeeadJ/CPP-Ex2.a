ab46464c0dc9ffc916a3a3812be03276e385e346
LeeadJ <leead123@gmail.com>
2022-03-22 20:11:43 +0200

    fixed checks

--- Notebook.cpp
+++ Notebook.cpp
@@ -8,22 +8,16 @@ namespace ariel{
         if(column > MAX_COL){
             throw runtime_error("Column input Exceeds the size Restriction!");
         }
-        else if(text.length() > MAX_COL && dir == Direction::Horizontal){
+        else if((text.length() > MAX_COL && dir == Direction::Horizontal) || (text.length() + column > MAX_COL && dir == Direction::Horizontal)){
             throw runtime_error("Text input Exceeds the Column size Restriction!");
-        }
-        else if((text.length() > column && dir == Direction::Horizontal) || (text.length() > row && dir == Direction::Vertical)){
-            throw runtime_error("Text input Exceeds the Column/Row input!");
         }
     }
     void erase(unsigned int page,unsigned int row,unsigned int column, Direction dir,unsigned int len){
         if(column > MAX_COL){
             throw runtime_error("Column input Exceeds the size Restriction!");
         }
-        else if(len > MAX_COL && dir == Direction::Horizontal){
+        else if((len > MAX_COL && dir == Direction::Horizontal) || (len + column > MAX_COL && dir == Direction::Horizontal)){
             throw runtime_error("Erasing length Exceeds the Column size Restriction!");
-        }
-        else if((len > column && dir == Direction::Horizontal) || (len > row && dir == Direction::Vertical)){
-            throw runtime_error("Erasing length Exceeds the Column/Row input!");
         }
 
     }
@@ -31,11 +25,8 @@ namespace ariel{
         if(column > MAX_COL){
             throw runtime_error("Column input Exceeds the size Restriction!");
         }
-        else if(len > MAX_COL && dir == Direction::Horizontal){
+        else if((len > MAX_COL && dir == Direction::Horizontal) || (len + column > MAX_COL && dir == Direction::Horizontal)){
             throw runtime_error("Reading length Exceeds the Column size Restriction!");
-        }
-        else if((len > column && dir == Direction::Horizontal) || (len > row && dir == Direction::Vertical)){
-            throw runtime_error("Reading length Exceeds the Column/Row input!");
         }
         return "Function read";
     }
